2aef0ea94c0d78b820d99cad8d286684b2049bc7
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-12 00:13:08 +0000

    step 4

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,8 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!')
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
-
-getRepoContributors("lighthouse","thomas", function error (err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-})
