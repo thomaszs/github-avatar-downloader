8cb654ce1c2c4a0d328eaea00b6add948957ba3c
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-12 03:41:26 +0000

    avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,37 @@
 var request = require('request');
+var fs = require('fs');
+var tokens = require('./secrets').GITHUB_TOKEN;
+
 console.log('Welcome to the GitHub Avatar Downloader!')
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': 'token ' + tokens
     }
   };
-
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
+  request(options, cb);
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  var parsedResult = JSON.parse(result);
-  for (var i =0; i < parsedResult.length; i++) {
-  console.log(parsedResult[i].avatar_url)
+function downloadImageByURL(url, filePath) {
+  request.get(url)               
+         .on('error', function (err) {                                  
+           throw err; 
+         })
+         .pipe(fs.createWriteStream(filePath));
+         return;    
   }
-});
+
+function getAllAvatars(err, response, body) {
+  var parsedResult = JSON.parse(body);
+  for (var i = 0; i < parsedResult.length; i++) {
+    downloadImageByURL(parsedResult[i].avatar_url, "./avatars/" + parsedResult[i].login + ".jpg")
+  }
+}
+
+
+getRepoContributors("jquery", "jquery", getAllAvatars);
+
+
